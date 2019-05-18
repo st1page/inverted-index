c994ada4230f212916f615a803deda6a9f10b1b4
stonepage <1245835950@qq.com>
2019-05-19 03:07:50 +0800

    InvertedList

--- InvertedList.hpp
+++ InvertedList.hpp
@@ -9,11 +9,35 @@ public:
     InvertedList():SortedList(){    }
     InvertedList(const InvertedList &other):SortedList(other){    }
     
-    void uni(const InvertedList &other){
-        iterator it = this.begin();
+    void uni(InvertedList &other){
+        n_ptr p = _head;
+        iterator it = other.begin();
+        while(p!=nullptr&&it!=other.end()){
+            if(p->data<*it) {
+                p = p->nxt;
+            } else if(p->data==*it){
+                p = p->nxt;
+                ++it;
+            } else {
+                _ins_pre(p,*it); ++it;
+            }
+        }
+        while(it!=other.end()){
+            _push_back(*it); ++it;
+        }
     }
-    void intersec(){
-
+    void intersec(InvertedList &other){
+        n_ptr p = _head;
+        iterator it = other.begin();
+        while(p!=nullptr&&it!=other.end()){
+            if(p->data<*it) {
+                p = p->nxt;
+                _erase(p->pre);
+            } else if(p->data==*it){
+                p = p->nxt;
+                ++it;
+            } 
+        }
     }
 };
 #endif
--- SortedList.hpp
+++ SortedList.hpp
@@ -4,19 +4,21 @@ template<class T>
 struct Node{
     T data;
     Node* nxt;
+    Node* pre;
     Node(){
-        nxt = nullptr;
+        nxt = pre = nullptr;
     }
     Node(T x){
         data = x;
-        nxt = nullptr;
+        nxt = pre = nullptr;
     }
     Node(const Node<T> &other){
         data = other.data;
         nxt = other.nxt;
+        pre = other.pre;
     }
     ~Node(){
-        nxt = nullptr;
+        nxt = pre = nullptr;
     }
 };
 template<class T>
@@ -26,7 +28,6 @@ protected:
     n_ptr _head;
     n_ptr _tail;
     int _size;
-    
     void _clear(){
         n_ptr p = _head;
         n_ptr tmp = nullptr;
@@ -37,32 +38,75 @@ protected:
         }
         _size = 0;
     }
+    void _ins_pre(n_ptr ptr, T x){
+        ++_size;
+        n_ptr p = new Node<T>(x);
+        if(ptr->pre==nullptr){
+            ptr->pre = p;
+            p->nxt = ptr;
+            _head = p;
+        } else {
+            ptr->pre->nxt = p;
+            p->pre = ptr->pre;
+            p->nxt = ptr;
+            ptr->pre = p; 
+        }
+    }
+    void _ins_nxt(n_ptr ptr, T x){
+        ++_size;
+        n_ptr p = new Node<T>(x);
+        if(ptr->nxt==nullptr){
+            ptr->nxt = p;
+            p->pre = ptr;
+            _tail = p;
+        } else {
+            ptr->nxt->pre = p;
+            p->nxt = ptr->nxt;
+            p->pre = ptr;
+            ptr->nxt = p;
+        }
+    }
     void _push_back(T x){
-        _size++; 
-        if(_head == nullptr) {
+        _size++;
+        if(_head==nullptr) {
             _head = _tail = new Node<T>(x);
         } else {
             _tail->nxt = new Node<T>(x);
+            _tail->nxt->pre = _tail;
             _tail = _tail->nxt;
         }
     }
-    void _insert(T x){
-        _size++;
-        n_ptr *p = &_head;
-        while((*p)!=nullptr && *(p)->data<=x) p = &((*p)->nxt);
-        if(*p==nullptr) {
-            *p = new Node<T>(x);
-            _tail = *p;
+    void _add(T x){
+        if(_head==nullptr) {
+            ++_size;
+            _head = _tail = new Node<T>(x);
+        } else if(_tail->data<x){
+            _push_back(x);
         } else {
-            n_ptr tmp = (*p)->nxt;
-            *p = new Node<T>(x);
-            p->nxt = tmp;
+            n_ptr p = _head;
+            for(;p!=nullptr;p++){
+                if(p->data>x) {
+                    _ins_pre(p,x);
+                    break;
+                }
+            }
         }
     }
-    int _find(T x){
+    n_ptr _find(T x){
         for(n_ptr p = _head; p!=nullptr; p = p->nxt) 
-            if(p->data == x) return 1;
-        return 0;
+            if(p->data == x) return p;
+        return nullptr;
+    }
+    void _erase(n_ptr ptr){
+        if(ptr->pre!=nullptr) ptr->pre->nxt = ptr->nxt;
+        else _head = ptr->nxt;
+        if(ptr->nxt!=nullptr) ptr->nxt->pre = ptr->pre;
+        else _tail = ptr->pre;
+        delete ptr;
+    }
+    void _erase(T x){
+        n_ptr ptr = _find(x);
+        if(ptr!=nullptr) _erase(ptr);
     }
 public:
     struct iterator{
@@ -70,10 +114,10 @@ public:
         iterator(){ ptr = nullptr;}
         iterator(n_ptr p){ ptr = p;}
         T& operator*()const{
-            return *ptr;
+            return ptr->data;
         }
         T* operator->()const{
-            return ptr;
+            return &(ptr->data);
         }
         iterator& operator++(){
             if(ptr!=nullptr) ptr = ptr->nxt;
@@ -107,7 +151,7 @@ public:
     SortedList(const SortedList<T> &other){
         _clear();
         for(iterator it = other.begin(); it!=other.end(); it++){
-            _push_back(it->data);
+            push_back(it->data);
         }
     }
     ~SortedList(){
@@ -120,7 +164,10 @@ public:
         return _head==nullptr;
     }
     void add(T x){
-        _insert(x);
+        _add(x);
+    }
+    void erase(T x){
+        _erase(x);
     }
     int find(T x){
         return _find(x);
